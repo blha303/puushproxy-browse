899350cbc9c455e7a6f75941d35e1f68988be546
blha303 <stevensmith.ome@gmail.com>
2013-09-08 09:49:21 -0400

    Added bad input check, github ribbon

--- index.php
+++ index.php
@@ -4,13 +4,13 @@ include('config.php');
 <html>
   <head>
     <style>
-      img {
+      img.thumb {
         width: auto;
         max-height: 100;
       }
       body {
         background-color: #000000;
-        text-color: #FFFFFF;
+        color: #FFFFFF;
       }
       table {
         background-color: #cccccc;
@@ -41,12 +41,12 @@ include('config.php');
     <title>puush browse</title>
   </head>
   <body<?php if (!isset($_POST['email'])) echo " onLoad='document.getElementById(\"width\").value = GetWidth(); document.getElementById(\"email\").focus();'"; ?>>
+    <a href="https://github.com/blha303/puushproxy-browse">
+      <img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_white_ffffff.png" alt="Fork me on GitHub">
+    </a>
 <?php
 
-if (isset($_POST['email']) && isset($_POST['pass'])) { ?>
-  <table border=1>
-    <tr>
-<?php
+if (isset($_POST['email']) && isset($_POST['pass'])) {
 
   if (!isset($_POST['width'])) {
     $width = 800 / 100;
@@ -66,6 +66,9 @@ if (isset($_POST['email']) && isset($_POST['pass'])) { ?>
       $theuser = $user;
     }
   }
+  if (!$theuser) {
+    die("Couldn't find a user matching that email/password combination.");
+  }
 // I have several puushproxy instances using one database with different urls, so
 // I have to correct URLs manually here. If you can suggest a better solution,
 // please open a ticket! https://github.com/blha303/puushproxy-browse/issues/new
@@ -77,7 +80,11 @@ if (isset($_POST['email']) && isset($_POST['pass'])) { ?>
 
   $collection = $db->files;
 
-  $cursor = $collection->find(array("owner" => $theuser['_id']));
+  $cursor = $collection->find(array("owner" => $theuser['_id'])); ?>
+
+  <table border=1>
+    <tr>
+<?php
 
   $x = 0;
   $newline = true;
@@ -89,7 +96,7 @@ if (isset($_POST['email']) && isset($_POST['pass'])) { ?>
     if (substr($document['name'], 0, 2) == "ss" && file_exists("upload/".$document['name'])) { ?>
       <td>
         <a href='http://<?php echo $domain; ?>/<?php echo $document['shortname']; ?>'>
-          <center><img src='imagethumb.php?s=upload/<?php echo $document['name']; ?>&w=100'></center>
+          <center><img src='imagethumb.php?s=upload/<?php echo $document['name']; ?>&w=100' class='thumb'></center>
         </a>
       </td>
 <?php
