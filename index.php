342b92bf4ed48e83b74186e1d744976f38b8ea43
Steven Smith <stevensmith.ome@gmail.com>
2013-11-12 02:14:11 +0800

    Extension support is now part of puushproxy
    as of https://github.com/mave/puushproxy/commit/76cdc6b0e11ef6ee9fde6ace4fd00f8a38999d77

--- index.php
+++ index.php
@@ -96,7 +96,7 @@ if (isset($_POST['email']) && isset($_POST['pass'])) {
       $x = 0;
     }
     if (substr($document['name'], 0, 2) == "ss" && file_exists("upload/".$document['name'])) {
-      #$ext = "." . pathinfo($document['name'], PATHINFO_EXTENSION); ?>
+      $ext = "." . pathinfo($document['name'], PATHINFO_EXTENSION); ?>
       <td>
         <a href='http://<?php echo $domain; ?>/<?php echo $document['shortname'].$ext; ?>' target="_blank">
           <center><img src='imagethumb.php?s=upload/<?php echo $document['name']; ?>&w=100' class='thumb'></center>
